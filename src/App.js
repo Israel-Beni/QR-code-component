5ea98dbfc7571575189a861e458d370bcedaa0b7
Israel-Beni <bendegbe72@gmail.com>
2022-02-20 02:10:24 +0530

    App.js scripts completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css";
+import qrImage from '../images/'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <div className="qr-container">
+          <img src={qrImage} alt="" />
+        </div>
+        <div className="instruction-container">
+          <p className="instruction"> Improve your font-end skills by building projects.</p>
+          <p className="detailled-instruction">
+            Scan the QR code to visit Fontend Mentor and take you coding skills to the next level.
+          </p>
+        </div>
+      </div>
+    );
+  }
+};
 
 export default App;
