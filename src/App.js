28a6fd8535637f381c2ce30921aa0a87f0840729
Israel-Beni <bendegbe72@gmail.com>
2022-02-20 03:00:55 +0530

    * Oufit font imported from fonts.google.com * App.css desktop styling completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./App.css";
-import qrImage from '../images/'
+import qrImage from './images/image-qr-code.png';
 
 class App extends React.Component {
   render() {
@@ -11,7 +11,7 @@ class App extends React.Component {
         </div>
         <div className="instruction-container">
           <p className="instruction"> Improve your font-end skills by building projects.</p>
-          <p className="detailled-instruction">
+          <p className="detailed-instruction">
             Scan the QR code to visit Fontend Mentor and take you coding skills to the next level.
           </p>
         </div>
